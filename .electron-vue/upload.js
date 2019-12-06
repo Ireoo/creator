808fa5bb09da66a3aa6410b47d3f111bc72767fa
Jack.Wang <jack.wang@integem.com>
2019-12-07 00:00:18 +0800

    优化打包后上传内容

--- .electron-vue/upload.js
+++ .electron-vue/upload.js
@@ -7,14 +7,19 @@ const consola = require("consola");
 const yaml = require("js-yaml");
 const zip = require("./libs/zip");
 
-const upload = (from, to) => {
+const upload = (from, to, always=true) => {
   return new Promise((resolve, reject) => {
     consola.info(`Starting upload ${from} to ${to} ...`);
     client.scp(from, to, function(err) {
       if (err) {
         // return reject(err);
-        console.log(err);
-        return upload(from, to);
+        if(always) {
+          console.log(err);
+          return upload(from, to);
+        } else {
+          consola.ready(`Upload ${from} to ${to} is Failed!`);
+          resolve();
+        }
       } else {
         consola.ready(`Upload ${from} to ${to} is OK!`);
         resolve();
@@ -29,37 +34,25 @@ const upload = (from, to) => {
     switch (os.platform()) {
       case "darwin":
         file = `${package.build.productName}-${package.version}.dmg`;
-        zipFile = `${package.build.productName}-${package.version}-mac.zip`;
+        zipFile = `${package.build.productName}_mac_${package.version}.zip`;
+        await zip(path.join(process.cwd(), "build/mac"), path.join(process.cwd(), `build/${zipFile}`));
         break;
 
       case "win32":
         file = `${package.build.productName} Setup ${package.version}.exe`;
         zipFile = `${package.build.productName}_win_${package.version}.zip`;
-        if (!fs.existsSync(path.join(process.cwd(), `build/${zipFile}`))) {
-          consola.info(`Starting zip win files...`);
-          await zip(
-            path.join(process.cwd(), "build/win-unpacked"),
-            path.join(process.cwd(), `build/${zipFile}`)
-          );
-          consola.info(`Ziping files is OK!`);
-        } else {
-          consola.info(`zip files is exists!`);
-        }
+        consola.info(`Starting zip win files...`);
+        await zip(path.join(process.cwd(), "build/win-unpacked"), path.join(process.cwd(), `build/${zipFile}`));
         break;
 
       default:
         file = `${package.name}_${package.version}_amd64.deb`;
         zipFile = `${package.build.productName}_linux_${package.version}.zip`;
-        if (!fs.existsSync(path.join(process.cwd(), `build/${zipFile}`))) {
-          consola.info(`Starting zip linux files...`);
-          await zip(
+        consola.info(`Starting zip linux files...`);
+        await zip(
             path.join(process.cwd(), "build/linux-unpacked"),
             path.join(process.cwd(), `build/${zipFile}`)
-          );
-          consola.info(`Ziping files is OK!`);
-        } else {
-          consola.info(`zip files is exists!`);
-        }
+        );
         break;
     }
     // let file = os.platform() === "win32" ? `iPic Setup ${package.version}.exe` : `iPic-${package.version}.dmg`;
@@ -68,23 +61,24 @@ const upload = (from, to) => {
     //   consola.info(`Starting zip win files...`);
     //   await zip(path.join(process.cwd(), "build/win-unpacked"), path.join(process.cwd(), `build/${zipFile}`));
     // }
+    await upload(
+        `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/info.yml`,
+        path.join(process.cwd(), `build/info.yml`)
+    );
+
+    let info = yaml.safeLoad(fs.readFileSync(path.join(process.cwd(), `build/info.yml`), "utf8"));
+
     if (process.argv[2] || process.argv[2] === "all") {
-      await upload(
-        path.join(process.cwd(), `build/${file}`),
-        `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/`
+      if(fs.existsSync(path.join(process.cwd(), `build/${file}`)) && info.download[`${os.platform()}`] !== file) await upload(
+          path.join(process.cwd(), `build/${file}`),
+          `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/`
       );
-      await upload(
-        path.join(process.cwd(), `build/${zipFile}`),
-        `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/`
+      if(fs.existsSync(path.join(process.cwd(), `build/${zipFile}`)) && info.download[`${os.platform()}_zip`] !== zipFile) await upload(
+          path.join(process.cwd(), `build/${zipFile}`),
+          `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/`
       );
     }
-    await upload(
-      `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/info.yml`,
-      path.join(process.cwd(), `build/info.yml`)
-    );
-    let info = yaml.safeLoad(
-      fs.readFileSync(path.join(process.cwd(), `build/info.yml`), "utf8")
-    );
+
     consola.ready(`Starting process info.yml ...`);
     switch (os.platform()) {
       case "win32":
@@ -103,14 +97,11 @@ const upload = (from, to) => {
         break;
     }
     consola.info(yaml.safeDump(info));
-    fs.writeFileSync(
-      path.join(process.cwd(), `build/info.yml`),
-      yaml.safeDump(info)
-    );
+    fs.writeFileSync(path.join(process.cwd(), `build/info.yml`), yaml.safeDump(info));
     consola.ready(`Process info.yml is OK!`);
     await upload(
-      path.join(process.cwd(), `build/info.yml`),
-      `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/info.yml`
+        path.join(process.cwd(), `build/info.yml`),
+        `root:cc880108@player.integem.com:/home/tools/${package.build.productName}/info.yml`
     );
   } catch (e) {
     consola.error(e);
