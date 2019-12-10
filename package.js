d2bc52f05502ae5d0795f9b1caf27f4ac9a80322
Jack.Wang <jack.wang@integem.com>
2019-12-10 10:14:14 +0800

    Update package.js

--- package.js
+++ package.js
@@ -6,4 +6,4 @@ let version = package.version
 let versions = version.split('.')
 versions[versions.length - 1]++
 package.version = versions.join('.')
-fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(package, null, 4))
+fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(package, null, 2))
